4424131aeafd7e0c0941903e5f1cfe2250455dbb
JiaG <giau32002@gmail.com>
2023-11-18 19:45:11 +0700

    finish timer screen

--- api/README.md
+++ api/README.md
@@ -1,2 +1,2 @@
 #1 npm install json-server
-#2 json-server --watch db.json
+#2 json-server --watch db.json --port 3000
--- screens/Timer.js
+++ screens/Timer.js
@@ -1,62 +1,99 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View, FlatList, Pressable, SafeAreaView, ScrollView } from 'react-native'
 import ITEMDETAILS from '../data/ItemDetails'
 import BannerPack from '../components/BannerPack'
-import ItemDetails from '../components/ItemDetails'
 import ListAuthorDuration from '../components/ListAuthorDuration'
 import DURATIONS from '../data/DurationsData'
 import DownloadTimer from '../components/DownloadTimer'
 
 
 export default function Timer({ navigation }) {
+  const [dataTimer, setDataTimer] = useState({});
   const [selectedData, setSelectedData] = useState({});
 
   const handleSelectedDataDuration = (data) => {
     setSelectedData(data);
+    console.log(dataTimer);
   };
 
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        scrollEnabled={true}
-        style={styles.container1}
-        scrollIndicatorInsets={{
-          bottom: 50,
-        }}
-        contentContainerStyle={{
-          marginBottom: 100,
-        }}
-      >
-        <View style={styles.banner}>
-          <BannerPack
-            title={ITEMDETAILS[0].title}
-            subtile={ITEMDETAILS[0].subtile}
-            onPress={() => { }}
-            image='50x50png'
-          >
-          </BannerPack>
-        </View>
-        <SafeAreaView style={{ flex: 1 }}>
-          <ListAuthorDuration data={DURATIONS} onSelect={handleSelectedDataDuration}></ListAuthorDuration>
-        </SafeAreaView>
-        <View style={styles.download}>
-          <DownloadTimer author='JiaG' time='9'></DownloadTimer>
-        </View>
+  /* const ITEMDETAILS_URL = 'http://localhost:3000/ITEMDETAILS';
+  const DURATIONS_URL = 'http://localhost:3000/DURATIONS';
 
-      </ScrollView>
+  const getApi = (url, state, setState, nameField) => {
+    const getDataFromApi = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('network error');
+        }
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error('Error get api:', error);
+        return null;
+      }
+    };
 
-      <Pressable
-        android_ripple={{ color: '#9eb9f7', borderless: false }}
-        onPress={() => navigation.navigate('Player')}
-        style={({ pressed }) => [
-          { backgroundColor: pressed ? '#9eb9f7' : '#f4f5b0' }
-          , styles.beginButton
-        ]}
-      >
-        <Text style={styles.textBeginButton}>Begin</Text>
-      </Pressable>
-    </View>
-  )
+    getDataFromApi()
+      .then((data) => {
+        console.log('Data from API:', data);
+        setState({ ...state, "ITEMDETAILS": {...data[0]} });
+  })
+      .catch ((error) => {
+    console.error('Error:', error);
+  });
+}
+const a = "ITEMSDETAILS";
+useEffect(() => {
+  getApi(ITEMDETAILS_URL, dataTimer, setDataTimer, a);
+  console.log(dataTimer);
+}, []);
+ */
+
+
+
+return (
+  <View style={styles.container}>
+    <ScrollView
+      scrollEnabled={true}
+      style={styles.container1}
+      scrollIndicatorInsets={{
+        bottom: 50,
+      }}
+      contentContainerStyle={{
+        marginBottom: 100,
+      }}
+    >
+      <View style={styles.banner}>
+        <BannerPack
+          title={ITEMDETAILS[0].title}
+          subtile={ITEMDETAILS[0].subtile}
+          onPress={() => { }}
+          image='50x50png'
+        >
+        </BannerPack>
+      </View>
+      <SafeAreaView style={{ flex: 1 }}>
+        <ListAuthorDuration data={DURATIONS} onSelect={handleSelectedDataDuration}></ListAuthorDuration>
+      </SafeAreaView>
+      <View style={styles.download}>
+        <DownloadTimer author='JiaG' time='9'></DownloadTimer>
+      </View>
+
+    </ScrollView>
+
+    <Pressable
+      android_ripple={{ color: '#9eb9f7', borderless: false }}
+      onPress={() => navigation.navigate('Player')}
+      style={({ pressed }) => [
+        { backgroundColor: pressed ? '#9eb9f7' : '#f4f5b0' }
+        , styles.beginButton
+      ]}
+    >
+      <Text style={styles.textBeginButton}>Begin</Text>
+    </Pressable>
+  </View>
+)
 }
 
 const styles = StyleSheet.create({
